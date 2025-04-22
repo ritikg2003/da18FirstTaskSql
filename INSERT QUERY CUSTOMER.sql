3961f766dee172094a5cedee8b469f2b06950143
ritikg2003 <ritikgathibandhe66@gmail.com>
2025-04-22 23:12:06 +0530

    ritik gathibandhe.: my first sql task insert query

---
+++ INSERT QUERY CUSTOMER.sql
@@ -0,0 +1,24 @@
+select *from customer
+
+
+insert into customer(name,mob_no,state,city,pincode,age)values('Ritik',9049496689,'maharashtra','nagpur',440017,21)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('akash',9049495589,'maharashtra','pune',444002,24)
+
+insert into customer(name,state,city,pincode,age)values('divya','maharashtra','mumbai','440006','25')
+
+insert into customer(name,mob_no,state,city,pincode,age)values('mayuri',7896541230,'maharashtra','pune',785464,21)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('ganesh','8974563210','maharashtra','kokan',78542,20)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('rajat','9632587410','maharashtra','pune',789654,21)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('roshan','8521479630','maharashtra','mumbai',440012,31)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('devansh','7418529630','maharashtra','koakn',440014,30)
+
+insert into customer(name,state,city,pincode,age)values('avinash','maharashtra','mumbai',784450,35)
+
+insert into customer(name,mob_no,state,city,pincode,age)values('vicky','789654130','maharashtra','pune',798454,30)
+
+select *from customer
